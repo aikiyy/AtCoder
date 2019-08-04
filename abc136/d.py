c71e1f9a91fa5305767f54f2f395c98893a5cc75
aikiyy <aiki.yougai@gmail.com>
2019-08-04 22:07:47 +0900

    modify & solve abc136 d

--- abc136/d.py
+++ abc136/d.py
@@ -1,29 +1,46 @@
 # -*- coding: utf-8 -*-
 
 
-def move(s, score):
-    new_score = [0] * len(s)
-    for i, v in enumerate(score):
-        if v == 0:
-            continue
-        if s[i] == 'R':
-            new_score[i + 1] += v
-        else:
-            new_score[i - 1] += v
-    return new_score
-
-
 if __name__ == '__main__':
-    s = input()
-    score = _score = [1] * len(s)
-    i = 0
-    while True:
-        tmp_score = score
-        score = move(s, score)
-        if score == _score:
-            if i % 2 == 0:
-                score = move(s, score)
-            break
-        _score = tmp_score
-        i += 1
-    print(' '.join(map(str, score)))
+    S = input()
+    score = _score = [0] * len(S)
+    r_num = 0
+    l_num = 0
+    _s = None
+    change_idx = None
+    for i, s in enumerate(S):
+        if s == 'L' and _s == 'R':
+            change_idx = i
+        if s == 'R' and _s == 'L':
+            #計算
+            if r_num == l_num or (r_num + l_num) % 2 == 0:
+                score[change_idx - 1] = (r_num + l_num) / 2
+                score[change_idx] = (r_num + l_num) / 2
+            elif r_num % 2 == 0:
+                score[change_idx - 1] = (r_num + l_num) // 2
+                score[change_idx] = (r_num + l_num) // 2 + 1
+            else:
+                score[change_idx - 1] = (r_num + l_num) // 2 + 1
+                score[change_idx] = (r_num + l_num) // 2
+            #初期化
+            r_num = 0
+            l_num = 0
+            change_idx = None
+        if s == 'R':
+            r_num += 1
+        else:
+            l_num += 1
+        _s = s
+
+    #計算
+    if r_num == l_num or (r_num + l_num) % 2 == 0:
+        score[change_idx - 1] = (r_num + l_num) / 2
+        score[change_idx] = (r_num + l_num) / 2
+    elif r_num % 2 == 0:
+        score[change_idx - 1] = (r_num + l_num) // 2
+        score[change_idx] = (r_num + l_num) // 2 + 1
+    else:
+        score[change_idx - 1] = (r_num + l_num) // 2 + 1
+        score[change_idx] = (r_num + l_num) // 2
+
+    print(' '.join(map(str, map(int, score))))
