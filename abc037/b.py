68a4ce3fd459ccab922dd02e18738038625d919f
aikiyy <aiki.yougai@gmail.com>
2019-08-05 12:58:04 +0900

    add another solution abc037 b

--- abc037/b.py
+++ abc037/b.py
@@ -7,4 +7,5 @@ for _ in range(Q):
     l, r, t = map(int, input().split(' '))
     for i in range(l-1, r):
         res[i] = t
-print('\n'.join([str(i) for i in res]))
+#print('\n'.join([str(i) for i in res]))
+print(*res, sep='\n')
