63f5fc14f5230878f9eb005536288173221b82db
aikiyy <aiki.yougai@gmail.com>
2019-08-05 12:16:22 +0900

    solve abc031 b

---
+++ abc031/b.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+L, H = map(int, input().split(' '))
+N = int(input())
+A = [int(input()) for _ in range(N)]
+for a in A:
+    if a > H:
+        print(-1)
+    elif a < L:
+        print(L - a)
+    else:
+        print(0)
