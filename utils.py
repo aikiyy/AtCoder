e95c54fbd052bfafde7555c587e746599c2153af
aikiyy <aiki.yougai@gmail.com>
2019-08-11 12:41:53 +0900

    add Heapq in utils

---
+++ utils.py
@@ -0,0 +1,45 @@
+import heapq
+
+
+class Reverse:
+    def __init__(self, val):
+        self.val = val
+
+    def __lt__(self, other):
+        return self.val > other.val
+
+    def __repr__(self):
+        return repr(self.val)
+
+
+class Heapq:
+    def __init__(self, arr, desc=False):
+        if desc:
+            for i in range(len(arr)):
+                arr[i] = Reverse(arr[i])
+        self.desc = desc
+        self.hq = arr
+        heapq.heapify(self.hq)
+
+    def pop(self):
+        if len(self.hq) == 0:
+            return None
+        if self.desc:
+            return heapq.heappop(self.hq).val
+        else:
+            return heapq.heappop(self.hq)
+
+    def push(self, a):
+        if self.desc:
+            heapq.heappush(self.hq, Reverse(a))
+        else:
+            heapq.heappush(self.hq, a)
+
+    def top(self):
+        if self.desc:
+            return self.hq[0].val
+        else:
+            return self.hq[0]
+
+    def __len__(self):
+        return len(self.hq)
