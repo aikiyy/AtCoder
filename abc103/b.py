6e004aa716458f3915bf8b20b4a53917d35381c7
aikiyy <aiki.yougai@gmail.com>
2019-08-11 15:41:32 +0900

    add another solution abc103 b

--- abc103/b.py
+++ abc103/b.py
@@ -1,7 +1,9 @@
-s, t = [input() for _ in range(2)]
-for i in range(len(s)):
-    if s[i:] + s[:i] == t:
-        print('Yes')
-        break
-else:
-    print('No')
+# s, t = [input() for _ in range(2)]
+# for i in range(len(s)):
+#     if s[i:] + s[:i] == t:
+#         print('Yes')
+#         break
+# else:
+#     print('No')
+
+print('Yes' if input() in input()*2 else 'No')
