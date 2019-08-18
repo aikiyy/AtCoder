95647c71b3948beff24fe6a657f9e026df080d94
aikiyy <aiki.yougai@gmail.com>
2019-08-18 22:00:23 +0900

    add abc138 e wrong answer

--- abc138/e.py
+++ abc138/e.py
@@ -2,24 +2,20 @@ S = input()
 T = input()
 
 if not set(S) >= set(T):
-    print('-1')
+    print(-1)
     exit()
-
-idxs = list()
-for t in T:
-    idx = set()
-    for i, s in enumerate(S):
-        if t == s:
-            idx.add(i)
-    idxs.append(idx)
 
 str_repeat_num = 0
 now_idx = 0
-for i, t in enumerate(T):
-    l = list(filter(lambda x: x > now_idx, idxs[i]))
-    if len(l) != 0:
-        now_idx = l[0]
+for t in T:
+    idx = set()
+    for i, s in enumerate(S[now_idx:]):
+        if t == s:
+            now_idx = i + 1 + now_idx
+            break
     else:
         str_repeat_num += 1
-        now_idx = idxs[i].pop()
-print(len(S) * str_repeat_num + now_idx + 1)
+        for i, s in enumerate(S[:now_idx]):
+            if t == s:
+                now_idx = i
+print(len(S) * str_repeat_num + now_idx)
