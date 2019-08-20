820e61d9c84ed9bf8fb9e02893ea988eca279bd1
aikiyy <aiki.yougai@gmail.com>
2019-08-20 16:34:40 +0900

    solve abc107 c

---
+++ abc107/c.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+x = list(map(int, input().split()))
+ans = []
+for i in range(n-k+1):
+    l = x[i]
+    r = x[i+k-1]
+    ans.append(min(abs(l) + abs(r-l), abs(r) + abs(l-r)))
+print(min(ans))
