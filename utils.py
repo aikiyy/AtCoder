645953985a6f5543db8447e6198d3ea8868908a2
aikiyy <aiki.yougai@gmail.com>
2019-08-24 01:45:24 +0900

    add divisors method in utils

--- utils.py
+++ utils.py
@@ -70,3 +70,17 @@ def least_common_multiple(iter):
     '''
     lcm = lambda x, y: x*y//gcd(x, y)
     return reduce(lcm, iter)
+
+
+def make_divisors(n):
+    '''
+    nの約数リストを返す
+    '''
+    divisors = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n//i)
+    # divisors.sort()
+    return divisors
