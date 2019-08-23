a4a5062bcb81fb876ae391b06defae7aecc45b52
aikiyy <aiki.yougai@gmail.com>
2019-08-23 20:04:14 +0900

    solve abc077 c

---
+++ abc077/c.py
@@ -0,0 +1,12 @@
+import bisect
+
+
+N = int(input())
+A = sorted(list(map(int, input().split())))
+B = sorted(list(map(int, input().split())))
+C = sorted(list(map(int, input().split())))
+
+ans = 0
+for b in B:
+    ans += bisect.bisect_left(A, b) * (N - bisect.bisect_right(C, b))
+print(ans)
