10b63d3f066ababfcd1d6b0e7573a6bc769bbfd7
aikiyy <aiki.yougai@gmail.com>
2019-08-29 11:14:32 +0900

    solve abc051 c

---
+++ abc051/c.py
@@ -0,0 +1,9 @@
+sx, sy, tx, ty = map(int, input().split())
+
+ans = ''
+ans += 'U' * (ty - sy) + 'R' * (tx - sx)
+ans += 'D' * (ty - sy) + 'L' * (tx - sx)
+ans += 'L' + 'U' * (ty - sy + 1) + 'R' * (tx - sx + 1) + 'D'
+ans += 'R' + 'D' * (ty - sy + 1) + 'L' * (tx - sx + 1) + 'U'
+
+print(ans)
