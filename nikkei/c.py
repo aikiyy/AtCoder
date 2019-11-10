07630a8f1d335a9773b0043368ce0b7b13e20ca3
aikiyy <aiki.yougai@gmail.com>
2019-11-10 09:52:52 +0900

    update nikkei2019-2 c but WA

--- nikkei/c.py
+++ nikkei/c.py
@@ -4,6 +4,12 @@ from collections import Counter
 N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
+max_a = max(A)
+max_b = max(B)
+
+if max_a > max_b:
+    print('No')
+    exit()
 
 wrong_num = 0
 for i in range(N):
@@ -13,12 +19,19 @@ if wrong_num > N-2:
     print('No')
     exit()
 
-c_a = Counter(A)
-c_b = Counter(B)
+c_a = sorted(Counter(A).items())
+c_b = sorted(Counter(B).items())
 
-for k, v in c_b.items():
-    t = sum([v2 for k2, v2 in c_a.items() if k2 <= k])
-    if t < v:
+idx = 0
+total_a = 0
+total_b = 0
+len_c_a = len(c_a)
+for k, v in c_b:
+    while idx < len_c_a and c_a[idx][0] <= k:
+        total_a += c_a[idx][1]
+        idx += 1
+    total_b += v
+    if total_a < total_b:
         print('No')
         exit()
 print('Yes')
