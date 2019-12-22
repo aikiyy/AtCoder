3edd989b9eedc2aca2670306a229f8fe3b16c122
aikiyy <aiki.yougai@gmail.com>
2019-12-23 00:18:31 +0900

    solve abc148 e

--- abc148/e.py
+++ abc148/e.py
@@ -1,27 +1,11 @@
-def f(n):
-    if n < 2:
-        return 1
-    return n * f(n-2)
-
-
 N = int(input())
 
-# 奇数の場合0が登場することはない
 if N & 1:
     print(0)
 else:
-    keta = len(str(N))
+    k = 10
     ans = 0
-    num_list = [1]
-    for i in range(1, keta-1):
-        if i == 1:
-            num_list.append(num_list[-1] * 10)
-        else:
-            num_list.append(num_list[-1] * 10 + 1)
-        num_list[-1] += i+1
-    ans += num_list[-1]
-    str_num = str(N//10)[1:]
-    for i in range(len(str_num)):
-        a = str_num[i]
-        ans += num_list[-(i+2)] * int(a)
+    while k <= N:
+        ans += N // k
+        k *= 5
     print(ans)
